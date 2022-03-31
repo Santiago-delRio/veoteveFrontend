7afabe37cee5c188605fea0d61eef16b055d9189
Santiago-delRio <santiago.delrio313@gmail.com>
2022-03-31 00:43:02 -0300

    agregue google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,64 @@
-import Layout from "../components/Layout"
-import Head from 'next/head'
-import '../styles/globals.scss'
-import '../node_modules/swiper/swiper-bundle.css';
+import Layout from "../components/Layout";
+import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import "../styles/globals.scss";
+import "../node_modules/swiper/swiper-bundle.css";
+//Google analytics
+import * as ga from "../lib/google-analytics";
 
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-BX2PQKB939"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-BX2PQKB939');
+// </script>
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-      <Layout>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <meta name="description" content="Somos la nueva forma de informarte en General Rodríguez. Información con estilo moderno y fresco.
-          Programas en vivo, actualidad, radio y muchísimo más"/>
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-  )
+    <Layout>
+      {/* Google Analytics - gtag.js */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `}
+      </Script>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Somos la nueva forma de informarte en General Rodríguez. Información con estilo moderno y fresco.
+          Programas en vivo, actualidad, radio y muchísimo más"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
