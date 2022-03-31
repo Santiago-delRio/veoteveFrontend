2f3969326d5adb50325640dc8d5ec928f750950b
Santiago-delRio <santiago.delrio313@gmail.com>
2022-03-31 12:03:04 -0300

    arregle google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,27 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
 import Script from "next/script";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import "../styles/globals.scss";
 import "../node_modules/swiper/swiper-bundle.css";
-//Google analytics
-import * as ga from "../lib/google-analytics";
-
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-BX2PQKB939"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-
-//   gtag('config', 'G-BX2PQKB939');
-// </script>
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <Layout>
-      {/* Google Analytics - gtag.js */}
+      {/* Google Analytics */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-          `}
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
       </Script>
+
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
